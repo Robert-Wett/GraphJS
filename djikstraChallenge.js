d6d25ee86280d2b14e4cee43387e5b458ef39ce4
Robert Wettlaufer <rdwettlaufer@gmail.com>
2014-07-12 03:32:56 +0000

    cleanup

--- djikstraChallenge.js
+++ djikstraChallenge.js
@@ -1,34 +1,42 @@
 var fs         = require('fs')
   , _          = require('underscore')
+  , program    = require('commander')
   , Graph      = require('./src/graph.js').Graph
   , Vertex     = require('./src/graph.js').Vertex
   , Connection = require('./src/graph.js').Connection;
 
-
-var parseInput = function(path, callback) {
-  var boardSize
+var buildGraphFromInput = function(path, callback) {
+  var filePath = path || './input.txt'
+    , g        = new Graph()
+    , alpha    = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    , boardSize
     , boardDef
     , nodePath
-    , boardArray = []
-    , filePath   = path || './input.txt'
-    , g          = new Graph()
-    , alpha      = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    , rowName
+    , colName
+    , weight;
 
   fs.readFile(filePath, function (err, data) {
     if (err) console.log(err);
 
-    boardDef  = String(data).split("\n");
+    boardDef  =  String(data).split("\n");
     boardSize = +boardDef.splice(0, 1);
-    nodePath  = boardDef.splice(-1, 1);
+    nodePath  =  boardDef.splice(-1, 1);
+
+    // Add the start/end nodes as properties to the graph
+    g.setProp('start', nodePath[0]);
+    g.setProp('end',   nodePath[1]);
+
     _.each(boardDef, function(row, idx) {
       boardDef[idx] = row.split(',');
     });
 
     _.each(_.range(boardSize), function(row) {
       _.each(_.range(boardSize), function(col) {
-        var rowName = alpha[row];
-        var colName = alpha[col];
-        var weight  = +boardDef[row][col];
+        rowName =  alpha[row];
+        colName =  alpha[col];
+        weight  = +boardDef[row][col];
+
         if (rowName !== colName && weight !== -1) {
           g.addEdge(rowName, colName, weight);
         }
@@ -39,7 +47,32 @@ var parseInput = function(path, callback) {
   });
 }
 
-function buildBoard(boardData, g) {
+
+buildGraphFromInput("./data/largeinput.txt", function(g) {
+  _.each(g.vertList, function(vert) {
+    console.log("\nId: ", vert.getId());
+
+    _.each(vert.getConnections(), function(con) {
+      console.log("Connected to", con.getId(), "with cost of ", con.cost);
+    });
+  });
+});
+
+
+/*
+// Non-callback implementation
+function parseInputText(path) {
+  var graph;
+  path = path || './input.txt';
+
+  fs.readFile(path, function(err, data) {
+    if (err) throw new Error(err);
+
+    graph = buildGraph(data);
+  });
+}
+
+function buildGraph(boardData, g) {
   var  boardDef  = String(boardData).split("\n")
     ,  boardSize = +boardDef.splice(0, 1)
     ,  nodePath  = boardDef.splice(-1, 1)
@@ -66,12 +99,4 @@ function buildBoard(boardData, g) {
 
   return graph;
 }
-
-parseInput("./largeinput.txt", function(g) {
-  _.each(g.vertList, function(vert) {
-    console.log("\nId: ", vert.getId());
-    _.each(vert.getConnections(), function(con) {
-      console.log("Connected to", con.getId(), "with cost of ", con.cost);
-    });
-  });
-});
+*/
